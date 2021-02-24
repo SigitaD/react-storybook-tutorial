a25dd23b6a0fba13ba8e96219400fb7de32d88d3
Sigita Dzedzickaite <sigita.dze@gmail.com>
2021-02-24 13:27:11 +0200

    Add theme

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
 import "./Button.css";
 
 function Button(props) {
-    const {variant = 'primary', children, ...rest} = props
-    return (
-        <button className={`button ${variant}`} {...rest}>{children}</button>
-    )
+  const { variant = "primary", children, ...rest } = props;
+  return (
+    <button className={`button ${variant}`} {...rest}>
+      {children}
+    </button>
+  );
 }
+
+Button.propTypes = {
+  variant: PropTypes.string,
+  children: PropTypes.object,
+};
 
 export default Button;
--- src/components/Center/Center.jsx
+++ src/components/Center/Center.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-import './Center.css'
+import "./Center.css";
 
 function Center({ children }) {
-    return (
-        <div className='center'>
-            {children}
-        </div>
-    )
+  return <div className="center">{children}</div>;
 }
+
+Center.propTypes = {
+  children: PropTypes.object,
+};
 
 export default Center;
